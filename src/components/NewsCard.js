bd7a2931ff4c4f1ecf4e0fef525b8b1330d71775
Alvin Quijano <alvin.quijano@live.ca>
2019-02-07 14:02:14 -0500

    updated styling in NewsCard component

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -44,35 +44,4 @@ class NewsCard extends React.Component {
   }
 }
 
-// const NewsCard = props => {
-//   return (
-//     <div>
-//       <Card
-//         className="news-card"
-//         onClick={() => {
-//           window.open(props.link);
-//         }}
-//       >
-//         <Col span={4} value={100}>
-//           <div className="article-image__container" />
-//         </Col>
-//         <Col value={100}>
-//           <h1>{props.title}</h1>
-//           <Row gutter={16}>
-//             <Col className="gutter-row" span={5}>
-//               <h4>{props.source}</h4>
-//             </Col>
-//             <Col className="gutter-row" span={8}>
-//               <h4>{props.date}</h4>
-//             </Col>
-//           </Row>
-//           <Row>
-//             <p>{props.description}</p>
-//           </Row>
-//         </Col>
-//       </Card>
-//     </div>
-//   );
-// };
-
 export default NewsCard;
